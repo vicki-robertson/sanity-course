948662dfcfd464454ece847507b408845bf31186
Vicki Robertson <vicki-robertson@users.noreply.github.com>
2025-07-13 00:42:08 +0200

    define additional eventType fields

--- apps/studio/schemaTypes/eventType.ts
+++ apps/studio/schemaTypes/eventType.ts
@@ -9,5 +9,44 @@ export const eventType = defineType({
       name: 'name',
       type: 'string',
     }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+    }),
+    defineField({
+      name: 'eventType',
+      type: 'string',
+    }),
+    defineField({
+      name: 'date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'doorsOpen',
+      type: 'number',
+    }),
+    defineField({
+      name: 'venue',
+      type: 'reference',
+      to: [{type: 'venue'}],
+    }),
+    defineField({
+      name: 'headline',
+      type: 'reference',
+      to: [{type: 'artist'}],
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'details',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'tickets',
+      type: 'url',
+    }),
   ],
 })
