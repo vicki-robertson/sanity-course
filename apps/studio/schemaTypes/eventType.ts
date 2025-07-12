d73044e1fed6519c3f10a5c09ad944b3d27010b6
Vicki Robertson <vicki-robertson@users.noreply.github.com>
2025-07-13 01:07:15 +0200

    add validation and group fields to eventType.ts. install sanity icons

--- apps/studio/schemaTypes/eventType.ts
+++ apps/studio/schemaTypes/eventType.ts
@@ -4,49 +4,80 @@ export const eventType = defineType({
   name: 'event',
   title: 'Event',
   type: 'document',
+  groups: [
+    {name: 'details', title: 'Details'},
+    {name: 'editorial', title: 'Editorial'},
+  ],
   fields: [
     defineField({
       name: 'name',
       type: 'string',
+      group: ['details', 'editorial'],
     }),
     defineField({
       name: 'slug',
       type: 'slug',
+      options: {source: 'name'},
+      group: 'details',
+      validation: (rule) => rule.required().error(`Required to generate a page on the website`),
+      hidden: ({document}) => !document?.name,
     }),
     defineField({
       name: 'eventType',
       type: 'string',
+      group: 'details',
+      options: {
+        list: ['in-person', 'virtual'],
+        layout: 'radio',
+      },
     }),
     defineField({
       name: 'date',
       type: 'datetime',
+      group: 'details',
     }),
     defineField({
       name: 'doorsOpen',
+      description: 'Number of minutes before the start time for admission',
       type: 'number',
+      initialValue: 60,
+      group: 'details',
     }),
     defineField({
       name: 'venue',
       type: 'reference',
+      group: 'details',
       to: [{type: 'venue'}],
+      readOnly: ({value, document}) => !value && document?.eventType === 'virtual',
+      validation: (rule) =>
+        rule.custom((value, context) => {
+          if (value && context?.document?.eventType === 'virtual') {
+            return 'Only in-person events can have a venue'
+          }
+          return true
+        }),
     }),
     defineField({
       name: 'headline',
       type: 'reference',
+      group: 'details',
       to: [{type: 'artist'}],
     }),
     defineField({
       name: 'image',
       type: 'image',
+      group: 'editorial',
     }),
     defineField({
       name: 'details',
       type: 'array',
+      group: 'editorial',
       of: [{type: 'block'}],
     }),
     defineField({
       name: 'tickets',
       type: 'url',
+      group: 'details',
     }),
   ],
 })
